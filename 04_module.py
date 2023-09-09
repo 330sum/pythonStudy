c0ac7156d487285ce6faa82bfb553fd94614f3f2
330sum <330sum@naver.com>
2023-09-09 15:52:19 +0900

    # 7.1 모듈
    7.1.1 모듈 만들기
    7.1.2 모듈 불러오기

---
+++ 04_module.py
@@ -0,0 +1,11 @@
+# 7.1.2 모듈 불러오기
+import diva
+
+# diva 모듈을 이용한 song() 메서드 실행
+diva.Singer.song("적외선카메라")
+
+# Singer 클래스 사용
+singer = diva.Singer()
+print(singer.name)
+
+
---
+++ diva.py
@@ -0,0 +1,7 @@
+# 7.1.1 모듈 만들기
+class Singer:
+    def __init__(self, name="Bambi"):
+        self.name = name
+
+    def song(title="No Name"):
+        print("Sing the", title)
