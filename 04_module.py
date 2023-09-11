a3d46b7148e463079c3ae456cc23934da9aeb56a
330sum <330sum@naver.com>
2023-09-11 16:11:46 +0900

    # 7.1 모듈
    7.1.3 특정함수나 클래스만 불러오기

--- 04_module.py
+++ 04_module.py
@@ -8,4 +8,15 @@ diva.Singer.song("적외선카메라")
 singer = diva.Singer()
 print(singer.name)
 
+# 7.1.3 특정함수나 클래스만 불러오기
+# calculator 모듈의 add() 함수만 사용
+from calculater import add
 
+print(add(3, 9))
+
+# 모듈의 모든 내용 가져오기
+from calculater import *
+
+print(add(4, 5))
+print(mul(3, 4))
+print(div(2, 2))
