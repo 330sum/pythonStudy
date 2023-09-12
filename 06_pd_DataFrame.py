23ef0b6952d86c4b53dbab35ab403bed90fa0177
330sum <330sum@naver.com>
2023-09-12 09:03:12 +0900

    # 팬더스 - DataFrame 데이터 타입 만들기

---
+++ 06_pd_DataFrame.py
@@ -0,0 +1,48 @@
+# 22개의 행과 4개의 열을 갖는 딕셔너리를 정의
+import pandas as pd
+
+heroes = {
+    "name": [
+        'ana', 'bastion', 'dva',
+        'genji', 'hanjo', 'junkrat',
+        'lucio', 'macree', 'mei',
+        'mercy', 'pharah', 'reaper',
+        'reinhardt', 'roadhog', 'soldier76',
+        'symmetra', 'torbjorn', 'tracer',
+        'widowmaker', 'winston', 'zarya',
+        'zenyatta'
+    ],
+    "health": [
+        200, 300, 500,
+        200, 200, 200,
+        200, 200, 250,
+        200, 200, 250,
+        500, 600, 200,
+        200, 200, 150,
+        200, 500, 400,
+        200
+    ],
+    "position": [
+        "support", "defense", "tank",
+        "offense", "defense", "defense",
+        "support", "offense", "defense",
+        "support", "offense", "offense",
+        "tank", "tank", "offense",
+        "support", "defense", "offense",
+        "defense", "tank", "tank",
+        "support"
+    ],
+    "id": [
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9,
+        10, 11, 12,
+        13, 14, 15,
+        16, 17, 18,
+        19, 20, 21,
+        22
+    ]
+}
+
+heroes_df = pd.DataFrame(heroes)
+print(heroes_df)
