9690913360084263c1fb5016cdc74fc24c303c97
330sum <330sum@naver.com>
2023-09-13 09:56:56 +0900

    # 팬더스 - 엑셀 파일 불러와서 출력하기

--- 07_pd_loadfile.py
+++ 07_pd_loadfile.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# 1. CSV 파일
 # read_csv()를 이용해 CSV 파일을 불러온다
 file_path = "/Users/superstar_park/PycharmProjects/heroes.csv"
 csv_data = pd.read_csv(file_path)
@@ -16,3 +17,14 @@ cav_data2 = pd.read_csv(file_path, header=None, names=colums)
 
 # 열 제목이 있는 형태로 데이터 5개를 출력한다
 print(cav_data2.head())
+
+# 2. 엑셀 파일
+# pip install xlrd
+# pip install openpyxl
+
+# read_excel()을 이용해 엑셀 파일을 불러온다
+# 이 때 heroes 시트를 선택하도록 한다
+file_path_excel = "/Users/superstar_park/PycharmProjects/heroes.xlsx"
+heroes_excel = pd.read_excel(file_path_excel, "heroes")
+
+heroes_excel.head()
