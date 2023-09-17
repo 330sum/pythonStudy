5985a818bc2f63d8a390e96e46b3f892a2b73179
330sum <330sum@naver.com>
2023-09-17 23:52:12 +0900

    # 055-059. 예외처리

---
+++ study02/part02/055_try_except.py
@@ -0,0 +1,6 @@
+try:
+    print('안녕하세요')
+    print(param)
+    # 정의되지 않은 변수 호출 - 예외(Exception) 발생
+except:
+    print('예외가 발생했습니다')
---
+++ study02/part02/058_try_except_Exception_as_e.py
@@ -0,0 +1,4 @@
+try:
+    print(param)
+except Exception as e:
+    print(e)  # [출력] name 'param' is not defined
