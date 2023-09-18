2aa5ee51079fd27f66bac1b947ff84816b4b6ad8
330sum <330sum@naver.com>
2023-09-18 18:41:14 +0900

    # 072. 최대값, 최소값 구하기(max, min)

---
+++ study02/part03/072_max_min.py
@@ -0,0 +1,16 @@
+listdata = [9.96, 1.27, 5.07, 6.45, 8.38, 9.29, 4.93, 7.73, 3.71, 0.93]
+maxval = max(listdata)
+minval = min(listdata)
+print(maxval)  # [출력] 9.96
+print(minval)  # [출력] 0.93
+
+txt = 'Alotofthingsoccureachday'
+maxval = max(txt)
+minval = min(txt)
+print(maxval)  # [출력] y
+print(minval)  # [출력] A
+
+maxval = max(2 + 3, 2 * 3, 2 ** 3, 3 ** 2)
+minval = min('abz', 'a12')
+print(maxval)  # [출력] 9
+print(minval)  # [출력] a12
