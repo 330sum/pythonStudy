81ee0b8a8cf99ab508ab1f2c21fb5661d7483e4a
330sum <330sum@naver.com>
2023-09-26 19:28:03 +0900

    도움말 보기, 소스코드 보기

--- study02/part02/039_def.py
+++ study02/part02/039_def.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 def add_number(n1, n2):
     sum = n1 + n2
     return sum
@@ -12,3 +15,21 @@ print(ans)
 text1 = '대한민국'
 text2 = '만세'
 add_txt(text1, text2)
+
+data = range(1, 11)
+
+
+def average(data):
+    """리스트의 값을 받아 평균을 구하는 함수"""
+    avg = sum(data) / len(data)
+    return avg
+
+
+print(average(data))
+
+# 도움말 보기
+help(average)
+print(average.__doc__)
+# 소스코드 보기
+source_code = inspect.getsource(average)
+print(source_code)
